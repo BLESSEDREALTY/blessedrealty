5f4891057a073850b975d2a3779e3a3f51f73de7
BLESSEDREALTY <blessedrealtyinc.001@gmail.com>
2026-05-24 02:46:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const akwaIbomLGAs = [
 "Abak","Eastern Obolo","Eket","Esit Eket","Essien Udim","Etim Ekpo","Etinan","Ibeno","Ibesikpo Asutan","Ibiono Ibom","Ika","Ikono","Ikot Abasi","Ikot Ekpene","Ini","Itu","Mbo","Mkpat Enin","Nsit Atai","Nsit Ibom","Nsit Ubium","Obot Akara","Okobo","Onna","Oron","Oruk Anam","Udung Uko","Ukanafun","Uruan","Urue Offong/Oruko","Uyo"
 ];
 
-const crossriversLGAs = [
+const riversLGAs = [
 "Port Harcourt","Obio-Akpor","Okrika","Ogu-Bolo","Eleme","Tai","Gokana","Khana","Oyigbo","Opobo-Nkoro","Andoni","Bonny","Ikot Abasi","Degema","Asari-Toru","Akuku-Toru","Abuad-Odual","Ahoada West","Ogba-Eghema-Ndoni","Emohua","Ikwerre","Etche","Omuma"];
 
   
